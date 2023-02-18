d4cb73c01cc8ad46aaddc6319a56d4ce6e60c2e8
Ranit <ranit.das@appcodeindia.com>
2023-02-18 23:21:51 +0530

    + python testing

--- python/app.py
+++ python/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/.well-known/acme-challenge')
+@app.route('/.well-known/acme-challenge/challenge')
 def acme_challenge():
-    return 'Your ACME challenge response here!'
+    return '2b7e39f4343b137f9585676fc4cfac7af4b3d27c8a9a966ab07ea0a60cdbc58c'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
